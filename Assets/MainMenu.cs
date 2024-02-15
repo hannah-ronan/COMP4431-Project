e54bab90fb6458339b20edaf73c372132aa39e21
justine061 <justinefeltam061@gmail.com>
2024-02-15 15:10:38 -0500

    Changed mainMenuScene background

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -19,7 +19,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Level1Scene");
     }
 
     public void QuitGame()
