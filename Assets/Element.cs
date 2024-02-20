76fc771966457afec04887bb8b0b51d69b8b5a94
csc530 <77406318+csc530@users.noreply.github.com>
2024-02-20 12:45:21 -0500

    :bug: fix element colours

--- Assets/Element.cs
+++ Assets/Element.cs
@@ -26,11 +26,11 @@ public class Element
     {
         return element switch
         {
-            Type.Water => new Color(68, 124, 207),
+            Type.Water => new Color(68/255f, 124/255f, 207/255f),
             //Element.Water => new Color(118, 187, 78),
-            Type.Fire => new Color(230, 75, 75),
-            Type.Air => new Color(255, 255, 255),
-            Type.Earth => new Color(118, 187, 78),
+            Type.Fire => new Color(230/255f, 75/255f, 75/255f),
+            Type.Air => new Color(255/255f, 255/255f, 255/255f),
+            Type.Earth => new Color(118/255f, 187/255f, 78/255f),
             _ => throw new InvalidEnumArgumentException("Invalid Element"),
         };
     }
