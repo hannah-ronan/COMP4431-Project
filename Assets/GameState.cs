0e9607e6f3fc8f747d08902e4d186cbe78ad4205
csc530 <77406318+csc530@users.noreply.github.com>
2024-02-22 22:50:29 -0500

    ✨ add score
    not shown to the player but saved

--- Assets/GameState.cs
+++ Assets/GameState.cs
@@ -1,28 +1,46 @@
 using System;
 using System.Collections;
+using System.Linq;
 using Objects.key;
+using Objects.token;
+using UI.timer;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameState: MonoBehaviour
+public class GameState : MonoBehaviour
 {
-    private PlayerController[] Players { get; set; }
-    public bool IsComplete { get; private set; }
-    private Key[] Keys { get; set; }
+	private Key[] Keys { get; set; }
+	private Token[] Tokens { get; set; }
+	private PlayerController[] Players { get; set; }
+	private Timer Timer { get; set; }
 
-    private void Awake()
-    {
-        IsComplete = false;
-        Keys = FindObjectsOfType<Key>();
-        Players = FindObjectsOfType<PlayerController>();
-        StartCoroutine(nameof(GameOver));
-    }
+	public bool IsComplete { get; private set; }
 
-    private IEnumerator GameOver()
-    {
-        yield return new WaitUntil(() => Array.TrueForAll(Keys, key => key == null || key.Collected));
-        foreach(var player in Players)
-            player.active = false;
-        IsComplete = true;
-        //todo: show game over screen, score & next level
-    }
+	public string Level { get; set; }
+
+	public int CollectedKeys => Keys.Where(key => key == null || key.Collected).ToList().Count;
+	public int CollectedTokens => Tokens.Count(token => token == null || token.collected);
+	public int Score => Convert.ToInt32(Math.Round(Timer.ElpasedTime.TotalSeconds * (CollectedKeys + CollectedTokens)));
+
+	private void Awake()
+	{
+		IsComplete = false;
+		Keys = FindObjectsOfType<Key>();
+		Tokens = FindObjectsOfType<Token>();
+		Players = FindObjectsOfType<PlayerController>();
+		Timer = GetComponent<Timer>();
+		Level = SceneManager.GetActiveScene().name;
+		StartCoroutine(nameof(GameOver));
+	}
+
+	private IEnumerator GameOver()
+	{
+		yield return new WaitUntil(() => Array.TrueForAll(Keys, key => key == null || key.Collected));
+		foreach (var player in Players)
+			player.active = false;
+		IsComplete = true;
+		//todo: show game over screen, score & next level
+	}
+
+	private void SaveScore() => PlayerPrefs.SetInt(Level, Score);
 }
