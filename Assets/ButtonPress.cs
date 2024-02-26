c6bd0f2869baa3e04e70dcdde6641d0dea704b3b
Hannah Ronan <hannah.ronan6@gmail.com>
2024-02-25 21:20:29 -0500

    add player switching and sprites to level 2

--- Assets/ButtonPress.cs
+++ Assets/ButtonPress.cs
@@ -6,19 +6,23 @@ public class ButtonPress : MonoBehaviour
 
 {
     public bool isButtonPressed = false;
+    public Sprite UnpressedSprite;
+    public Sprite PressedSprite;
+    public SpriteRenderer spriteRenderer;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-       
         isButtonPressed = true;
         Debug.Log("Button is pressed ");
+        spriteRenderer.sprite = PressedSprite;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         isButtonPressed = false;
         Debug.Log("Leaving button");
+        spriteRenderer.sprite = UnpressedSprite;
     }
 
     public bool isPressed()
