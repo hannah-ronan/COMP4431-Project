842752e9c42766921ca7009b112e3d35813b2a6c
justine061 <justinefeltam061@gmail.com>
2024-03-05 13:47:54 -0500

    Added level3 and level4 scenes and navigation

--- Assets/LevelSelection.cs
+++ Assets/LevelSelection.cs
@@ -6,15 +6,27 @@ using UnityEngine.SceneManagement;
 public class LevelSelection : MonoBehaviour
 {
     [SerializeField] private AudioSource buttonClickAudio;
-    public void levelOne()
+    public void LevelOne()
     {
         SceneManager.LoadScene("Level1Scene");
         PlayButtonClickSound();
     }
 
-    public void levelTwo()
+    public void LevelTwo()
     {
         SceneManager.LoadScene("Level2Scene");
+        PlayButtonClickSound();
+    }
+
+    public void LevelThree()
+    {
+        SceneManager.LoadScene("Level3Scene");
+        PlayButtonClickSound();
+    }
+
+    public void LevelFour()
+    {
+        SceneManager.LoadScene("Level4Scene");
         PlayButtonClickSound();
     }
     private void PlayButtonClickSound()
