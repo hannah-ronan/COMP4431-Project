57a732bbfbfd2792ad7262d55f9ce0711de073ba
justine061 <justinefeltam061@gmail.com>
2024-03-05 23:58:22 -0500

    Added door to level4

---
+++ Assets/ButtonPushedDoor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPushedDoor : MonoBehaviour
+{
+    GameObject button1, button2, door;
+    ButtonPress buttonScript1, buttonScript2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button1 = GameObject.Find("Button1");
+        button2 = GameObject.Find("Button2");
+
+        door = GameObject.Find("Door");
+
+        buttonScript1 = button1.GetComponent<ButtonPress>();
+        buttonScript2 = button2.GetComponent<ButtonPress>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (buttonScript1.isPressed() || buttonScript2.isPressed())
+        {
+            door.SetActive(false);
+        }
+        else
+        {
+            door.SetActive(true);
+        }
+    }
+}
